b46a801a00f87521c09f3f64c0d71cb971f6cd3e
Marck Alvarez <>
2021-04-27 21:34:22 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+let numero1  = prompt("ingrese un numero: ");
+let numero2 = prompt("ingrese otro numero: ");
+if (numero1 == numero2){ 
+   console.log("Los números son iguales") 
+}else{ 
+   if (numero1 > numero2) { 
+      console.log("El primer número es mayor que el segundo") 
+   }else{ 
+      console.log("El primer número es menor que el segundo") 
+   } 
+}
